b216aab32a0922604e474327c332b4a9d18b7028
lelethuml <lelethumlinjana71@gmailcom>
2023-06-14 23:43:10 +0200

    message

--- frontend/myApp/src/app/app.module.ts
+++ frontend/myApp/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { AboutpageComponent } from './components/aboutpage/aboutpage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomepageComponent
+    HomepageComponent,
+    AboutpageComponent
   ],
   imports: [
     BrowserModule,
