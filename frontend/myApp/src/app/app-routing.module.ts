6bb0f40cf533e49b992f87ee23607a09cb48ddab
lelethu <lelethumlinjana71@gmail.com>
2023-06-15 11:57:13 +0200

    message

--- frontend/myApp/src/app/app-routing.module.ts
+++ frontend/myApp/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutpageComponent } from './components/aboutpage/aboutpage.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: 'homepage', pathMatch:'full'},
+  {path: 'homepage', component: HomepageComponent},
+  {path: 'aboutpage', component: AboutpageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
